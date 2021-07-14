9f3083e5cf0e66558df2b8d2f8aa46b527ab96cd
Ari Wiradana <49831298+ariwiradana@users.noreply.github.com>
2021-07-15 06:16:04 +0800

    s

--- assets/js/front-end/index-read-api.js
+++ assets/js/front-end/index-read-api.js
@@ -46,7 +46,7 @@ function roomType() {
                                         </div>
                                     </div>
 
-                                    <p class="room-type-desc">${item.desc.length > 200 ? item.desc.substring(0, 200) + '...' : item.desc}</p>
+                                    <p class="room-type-desc">${item.desc.length > 150 ? item.desc.substring(0, 150) + '...' : item.desc}</p>
                                     <a href="rooms/?type=${item.id}">
                                         <p class="explore"><small>Explore <span>&#10230;</span></small></p>
                                     </a>
