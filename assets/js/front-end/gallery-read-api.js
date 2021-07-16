eeda8d5bbe93548b195b56875bb216e8e6d6caa8
Ari Wiradana <49831298+ariwiradana@users.noreply.github.com>
2021-07-17 00:22:27 +0800

    updated

--- assets/js/front-end/gallery-read-api.js
+++ assets/js/front-end/gallery-read-api.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-	// loadFoto();
-	dummyImg();
+	loadFoto();
+	// dummyImg();
 });
 
 function loadFoto() {
--- assets/js/front-end/location-read-api.js
+++ assets/js/front-end/location-read-api.js
@@ -1,45 +1,152 @@
-$(document).ready(function () {
-	mapData()
-});
+$(document).ready(function () {})
 
-const gmaps = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCvC1wgMf7631iiv1o7kaNXcnswYQ9b59I&callback=Maps";
+
+const gmaps = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCvC1wgMf7631iiv1o7kaNXcnswYQ9b59I&callback=mapData";
 $.getScript(gmaps, function () {});
 
-let map;
-
-function Maps() {
-	map = new google.maps.Map(document.querySelector(".map-content"), {
-		center: {
-			lat: -8.448348883235509,
-			lng: 115.25334555611327
-		},
-		zoom: 16,
-	});
-}
 
 function mapData() {
+	startLoading();
 
 	$.ajax({
-		url: "https://api-karens-house.000webhostapp.com/read-room-type.php",
+		url: "https://api-karens-house.000webhostapp.com/read-location.php",
 		type: "get",
 		dataType: "json",
 		success: function (response) {
+			stopLoading();
 			if (response.status_code == 200) {
-				const data = response.data;
-				const facilities = response.data[0].facilities;
+				const karenslocation = {
+					lat: -8.44819898282695,
+					lng: 115.25355074537637
+				};
 
-				$.each(data, function (i, data) {
-					$('#location').append(`
+				const map = new google.maps.Map(document.querySelector(".map-content"), {
+					center: karenslocation,
+					zoom: 12,
+				});
+
+				const marker = new google.maps.Marker({
+					position: karenslocation,
+					map,
+					label: "K",
+					title: "Karens House"
+				});
+
+				let infoWindow = new google.maps.InfoWindow();
+
+				marker.addListener("click", () => {
+					infoWindow.close();
+					infoWindow.setContent(marker.getTitle());
+					infoWindow.open(marker.getMap(), marker);
+				});
+
+
+				$.each(response.data, function (i, data) {
+					let infoWindow = new google.maps.InfoWindow();
+					let pos = new google.maps.LatLng(data.lat, data.lon);
+
+					let marker = new google.maps.Marker({
+						position: pos,
+						map,
+						title: `${i + 1}. ${data.location_name}`,
+						label: `${i + 1}`,
+						optimized: false,
+					});
+
+					marker.addListener("click", () => {
+						infoWindow.close();
+						infoWindow.setContent(marker.getTitle());
+						infoWindow.open(marker.getMap(), marker);
+					});
+					$(`#location-all-tab`).append(`
 						<div class="map-location-item">
-							<span>${i + 1}. <span class="location-name">${data.title}</span></span>
+							<span>${i + 1}. <span class="location-name">${data.location_name}</span></span>
+							<i class="ml-2 fas ${data.jenis == 'food' ? 'fa-utensils' : data.jenis == 'temple' ? 'fa-gopuram' : 'fa-camera'}"></i>
 						</div>
 					`)
 				});
+
 			}
 		}
 	})
 
-	$('.map-location-item').find('.location-name').click(function () {
-		console.log($(this).text());
+	$('.nav-tabs .nav-item a').click(function () {
+		startLoading();
+		let id = $(this).attr('id');
+		$(`#location-${id}`).html('');
+
+		let urls;
+
+		if (id == 'all-tab') {
+			urls = "https://api-karens-house.000webhostapp.com/read-location.php"
+		} else {
+			urls = `https://api-karens-house.000webhostapp.com/read-location.php?jenis=${id.split('-')[0]}`
+		}
+
+		$.ajax({
+			url: urls,
+			type: "get",
+			dataType: "json",
+			success: function (response) {
+				if (response.status_code == 200) {
+					stopLoading();
+
+					const karenslocation = {
+						lat: -8.44819898282695,
+						lng: 115.25355074537637
+					};
+
+					const map = new google.maps.Map(document.querySelector(".map-content"), {
+						center: karenslocation,
+						zoom: 12,
+					});
+
+					const marker = new google.maps.Marker({
+						position: karenslocation,
+						map,
+						label: "K",
+						title: "Karens House"
+					});
+
+					let infoWindow = new google.maps.InfoWindow();
+
+					marker.addListener("click", () => {
+						infoWindow.close();
+						infoWindow.setContent(marker.getTitle());
+						infoWindow.open(marker.getMap(), marker);
+					});
+
+					$.each(response.data, function (i, data) {
+						let infoWindow = new google.maps.InfoWindow();
+						let pos = new google.maps.LatLng(data.lat, data.lon);
+
+						let marker = new google.maps.Marker({
+							position: pos,
+							map,
+							title: `${i + 1}. ${data.location_name}`,
+							label: `${i + 1}`,
+							optimized: false,
+						});
+
+						marker.addListener("click", () => {
+							infoWindow.close();
+							infoWindow.setContent(marker.getTitle());
+							infoWindow.open(marker.getMap(), marker);
+						});
+					});
+
+
+					$.each(response.data, function (i, data) {
+						$(`#location-${id}`).append(`
+							<div class="map-location-item">
+								<span>${i + 1}. <span class="location-name">${data.location_name}</span></span>
+								<i class="ml-2 fas ${data.jenis == 'food' ? 'fa-utensils' : data.jenis == 'temple' ? 'fa-gopuram' : 'fa-camera'}"></i>
+							</div>
+						`);
+					});
+				}
+			}
+		})
 	});
+
 }
