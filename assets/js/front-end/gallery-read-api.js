000ccfa339eae089fcc087ef84dc2d9e5dcaffed
Ari Wiradana <49831298+ariwiradana@users.noreply.github.com>
2021-07-18 13:32:48 +0800

    update

--- assets/js/front-end/gallery-read-api.js
+++ assets/js/front-end/gallery-read-api.js
@@ -1,20 +1,49 @@
 $(document).ready(function () {
-	loadFoto();
 	// dummyImg();
+	loadGallery();
 });
 
-function loadFoto() {
+
+function loadGallery() {
+	const id = "all";
+	ajaxLoad(id);
+
+	$('.nav-gallery .nav-item').click(function () {
+		let id = $(this).find('.nav-link').attr('href').split('#')[1];
+		ajaxLoad(id);
+	});
+}
+
+
+
+function ajaxLoad(id) {
 	startLoading();
+
 	$.ajax({
 		url: 'https://api-karens-house.000webhostapp.com/read-gallery-img.php',
 		type: 'get',
 		dataType: 'json',
 		success: function (response) {
 			if (response.status_code == 200) {
-				$.each(response.data, function (i, data) {
-					$('.gallery-content').append(`
-					<div class="gallery-item">
-						<a href="https://api-karens-house.000webhostapp.com/${data}" data-fancybox="gallery" data-aos="flip-left" data-aos-duration="800">
+				let data = "";
+				if (id == "all") {
+					data = response.data.all;
+				} else if (id == "front") {
+					data = response.data.front;
+				} else if(id == 'bedroom'){
+					data = response.data.bedroom;
+				} else if(id == 'living-room'){
+					data = response.data.livingroom;
+				} else if(id == 'bathroom'){
+					data = response.data.bathroom;
+				} else{
+					data = response.data.terrace;
+				}
+
+				$.each(data, function (i, data) {
+					$(`#gallery-${id}`).append(`
+					<div class="gallery-item" data-aos="fade-up" data-aos-duration="800">
+						<a href="https://api-karens-house.000webhostapp.com/${data}" data-fancybox="gallery">
 							<div class="gallery-overlay"></div>
 							<img class="gallery-img" src="https://api-karens-house.000webhostapp.com/${data}">
 							<div class="gallery-overlay-container">
