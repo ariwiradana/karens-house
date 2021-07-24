7b0ffd17e4fd9f7a7808ed9374ce915bf2f25f9e
Ari Wiradana <49831298+ariwiradana@users.noreply.github.com>
2021-07-24 13:21:43 +0800

    sada

--- assets/js/custom/booking.js
+++ assets/js/custom/booking.js
@@ -8,9 +8,9 @@ $('.booking').click(function () {
         success: function (response) {
             if (response.status_code == 200) {
                 let data = [
-                    ["booking.com", "https://www.booking.com/hotel/id/karen-house-ubud.id.html"],
-                    ["tiket.com", "https://www.tiket.com/hotel/indonesia/karen-house-ubud-310001603224194661"],
-                    ["Chambers-Hotes.fr", "https://www.chambres-hotes.fr/chambres-hotes_karen-house-ubud_ubud_h4439009_en.htm"]
+                    ["Booking.com", "https://www.booking.com/hotel/id/karen-house-ubud.id.html"],
+                    ["Tiket.com", "https://www.tiket.com/hotel/indonesia/karen-house-ubud-310001603224194661"],
+                    ["Chambers-Hotes.fr", "https://www.chambres-hotes.fr/chambres-hotes_karen-house-ubud_ubud_h4439009_en.html"]
                 ];
 
                 // $.each(response.data, function (i, item) {
